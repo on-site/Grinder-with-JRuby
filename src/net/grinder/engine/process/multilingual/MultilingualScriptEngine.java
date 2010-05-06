9fb1dd3d2e158e4161728eb7cd5b02ff06a90a53
mikestone <stonemj@gmail.com>
2010-05-06 00:26:33 -0700

    Implement the multilingual script engine.

--- src/net/grinder/engine/process/multilingual/MultilingualScriptEngine.java
+++ src/net/grinder/engine/process/multilingual/MultilingualScriptEngine.java
@@ -21,9 +21,14 @@
 
 package net.grinder.engine.process.multilingual;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import net.grinder.engine.common.EngineException;
 import net.grinder.engine.common.ScriptLocation;
 import net.grinder.engine.process.ScriptEngine;
+import net.grinder.engine.process.jruby.JRubyScriptEngine;
+import net.grinder.engine.process.jython.JythonScriptEngine;
 
 
 /**
@@ -35,26 +40,78 @@ import net.grinder.engine.process.ScriptEngine;
  * @author Mike Stone
  */
 public final class MultilingualScriptEngine implements ScriptEngine {
+    private static final Map<String, Class<? extends ScriptEngine>> engineTypes;
+
+    static {
+        engineTypes = new HashMap<String, Class<? extends ScriptEngine>>();
+        engineTypes.put("py", JythonScriptEngine.class);
+        engineTypes.put("jy", JythonScriptEngine.class);
+        engineTypes.put("rb", JRubyScriptEngine.class);
+        engineTypes.put("jrb", JRubyScriptEngine.class);
+    }
+
+    private ScriptEngine engine;
+
     @Override
     public void initialise(ScriptLocation script) throws EngineException {
+        if (engine == null) {
+            setupEngine(script);
+        }
+
+        engine.initialise(script);
+    }
+
+    private void setupEngine(ScriptLocation script) throws EngineException {
+        String extension = getExtension(script);
+        Class<? extends ScriptEngine> type = engineTypes.get(extension);
+
+        if (type == null) {
+            throw new EngineException("Cannot find engine for extension " + extension);
+        }
+
+        try {
+            engine = type.newInstance();
+        } catch (InstantiationException e) {
+            throw new EngineException("Failed to instantiate engine of type " + type.getName() + " for extension " + extension, e);
+        } catch (IllegalAccessException e) {
+            throw new EngineException("Illegal access for engine of type " + type.getName() + " for extension " + extension, e);
+        }
+    }
+
+    private String getExtension(ScriptLocation script) {
+        String[] split = script.getFile().getName().split(".");
+
+        // No extension...
+        if (split.length < 2) {
+            return "";
+        }
+
+        return split[split.length - 1];
     }
 
     @Override
     public WorkerRunnable createWorkerRunnable() throws EngineException {
-        return null;
+        return engine.createWorkerRunnable();
     }
 
     @Override
     public WorkerRunnable createWorkerRunnable(Object testRunner) throws EngineException {
-        return null;
+        return engine.createWorkerRunnable(testRunner);
     }
 
     @Override
     public void shutdown() throws EngineException {
+        engine.shutdown();
     }
 
     @Override
     public String getDescription() {
-        return "Multilingual 0.0.1";
+        String version = "Multilingual 0.0.1";
+
+        if (engine != null) {
+            return version + ": " + engine.getDescription();
+        }
+
+        return version;
     }
 }
